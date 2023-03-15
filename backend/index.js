41bfa9327a07d68cd1861abc05b02d8506b6ccdc
Junior Love <107013570+junlov@users.noreply.github.com>
2023-03-15 17:37:22 -0400

    Part 3C - Follow Along Complete

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,9 @@ const Note = require("./models/note");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
 app.use(express.static("build"));
+app.use(express.json());
+app.use(cors());
 
 //
 //
@@ -24,35 +24,48 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger);
 
-//  ! Switched out for db id
-//  const generateId = () => {
-//   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-//   return maxId + 1;
-// };
-
 app.get("/", (request, response) => {
   response.send("<h1>Hello world!</h1>");
 });
 
+//
+// * GET ALL Route
+//
 app.get("/api/notes", (request, response) => {
   Note.find({}).then((notes) => {
     response.json(notes);
   });
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  console.log("ID", request.params.id);
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+//
+// * GET BY ID Route
+//
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+//
+// * DELETE Route
+//
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
+//
+// * POST Route
+//
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
@@ -73,6 +86,24 @@ app.post("/api/notes", (request, response) => {
 });
 
 //
+// * PUT Route
+//
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+//
 //
 // * Unknown Endpoint Middleware
 //
@@ -82,6 +113,23 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+//
+//
+// * Error Handler Middleware
+//
+//
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
