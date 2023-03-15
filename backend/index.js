5f173d9725a53fbf1e8ca1f01f75e70b8e208507
Junior Love <107013570+junlov@users.noreply.github.com>
2023-03-15 17:52:35 -0400

    Part 3d - Updated Validation

--- backend/index.js
+++ backend/index.js
@@ -66,7 +66,7 @@ app.delete("/api/notes/:id", (request, response, next) => {
 //
 // * POST Route
 //
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
 
   if (body.content === undefined) {
@@ -80,23 +80,25 @@ app.post("/api/notes", (request, response) => {
     important: `${body.important}` || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 //
 // * PUT Route
 //
 app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body;
+  const { content, important } = request.body;
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedNote) => {
       response.json(updatedNote);
     })
@@ -124,6 +126,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
