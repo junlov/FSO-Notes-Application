48f9a7e988ceeb452c7677a2ad04d7ce0aa26e7c
Junior Love <107013570+junlov@users.noreply.github.com>
2023-03-16 08:52:42 -0400

    Part 4a - Follow Along

---
+++ backend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    eqeqeq: "error",
+  },
+  // eslint-disable-next-line semi
+};
---
+++ backend/utils/middleware.js
@@ -0,0 +1,44 @@
+const logger = require("./logger");
+
+//
+// * Unknown Endpoint Middleware
+//
+//
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+//
+// * Error Handler Middleware
+//
+//
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+//
+//
+// * Request Logger Middlware
+//
+//
+const requestLogger = (request, response, next) => {
+  logger.info("Method:", request.method);
+  logger.info("Path:", request.path);
+  logger.info("Body:", request.body);
+  logger.info("----");
+  next();
+};
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+};
