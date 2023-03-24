833a9f8e0185504e69d4eb4e2afa5bbb4a101013
Junior Love <K4ngar0o@proton.me>
2023-03-24 09:00:40 -0500

    Testing the Backend Follow Along Complete

--- backend/tests/note_api.test.js
+++ backend/tests/note_api.test.js
@@ -8,9 +8,10 @@ const helper = require("./test_helper");
 beforeEach(async () => {
   await Note.deleteMany({});
 
-  const noteObjects = helper.initialNotes.map((note) => new Note(note));
-  const promiseArray = noteObjects.map((note) => note.save());
-  await Promise.all(promiseArray);
+  for (let note of helper.initialNotes) {
+    let noteObject = new Note(note);
+    await noteObject.save();
+  }
 });
 
 test("notes are returned as json", async () => {
